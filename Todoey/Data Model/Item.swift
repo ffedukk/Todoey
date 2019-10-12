e92686ebdf9eaa6d7c577e5d58917320b00f014c
Fedor <ffedukk@fedors-imac.local>
2019-10-12 11:01:52 -0700

    Convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Fedor on 12/10/2019.
+//  Copyright © 2019 Fedor Semenov. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
